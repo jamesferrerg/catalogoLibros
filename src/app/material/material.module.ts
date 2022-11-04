c30627e3a82bda0af1744b6ec1439a75554e2e77
jamesferrerg <jamesjfgv@gmail.com>
2022-11-03 20:10:39 -0500

    A. material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 
 
@@ -9,6 +10,7 @@ import { CommonModule } from '@angular/common';
     CommonModule,
   ],
   exports: [
+    MatCardModule,
   ]
 })
 export class MaterialModule { }
